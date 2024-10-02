3911432ed7a001a99f68aebbe5d0f1636e6f1980
IlirKK <kymori@outlook.de>
2024-10-02 22:25:50 +0200

    made home, contact and login responsive & re-designed login to have the same design as contact.

--- script/script.js
+++ script/script.js
@@ -4,8 +4,8 @@ const user1pw = "cbpw123";
 document.getElementById('loginForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    const username = document.getElementById('exampleInputEmail1').value;
-    const password = document.getElementById('exampleInputPassword1').value;
+    const username = document.getElementById("inputUsername").value;
+    const password = document.getElementById("inputPassword").value;
 
     if (username === user1 && password === user1pw) {
         window.location.href = "../index.html";
